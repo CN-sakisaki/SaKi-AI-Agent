3e487bf7e1e15109d0843efe513960e3907061ee
sakisaki <189444236@qq.com>
2025-06-27 15:45:37 +0800

    chore: initialize project with [framework/cli]

---
+++ SaKi-AI-Agent/src/main/java/com/saki/sakiaiagent/SaKiAiAgentApplication.java
@@ -0,0 +1,13 @@
+package com.saki.sakiaiagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaKiAiAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaKiAiAgentApplication.class, args);
+    }
+
+}
---
+++ SaKi-AI-Agent/src/test/java/com/saki/sakiaiagent/SaKiAiAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saki.sakiaiagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaKiAiAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
